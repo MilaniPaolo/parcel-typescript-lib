549baa21f142db38eaed036ad557158d1980c4a5
Milani Paolo <milani_paolo@outlook.it>
2022-04-07 23:19:41 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+const CONFIGCAT_MOCK = {
+  createClient: (sdkKey: string): string => 'connected!'
+};
+
+export default class ConfigCat {
+  protected static configCatClient: any;
+
+  init(sdkKey: string) {
+    ConfigCat.configCatClient = CONFIGCAT_MOCK.createClient(sdkKey);
+    return ConfigCat.configCatClient;
+  }
+}
